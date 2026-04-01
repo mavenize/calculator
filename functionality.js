992a260746a7fe1474880088304c0fe3861c4b49
marko <markok.kod@gmail.com>
2026-04-01 17:01:58 +0200

    Capture DOM values

---
+++ functionality.js
@@ -0,0 +1,26 @@
+
+const calculator = {
+    // numbers
+    one : document.getElementById("one").textContent,
+    two : document.getElementById("two").textContent,
+    three : document.getElementById("three").textContent,
+    four : document.getElementById("four").textContent,
+    five : document.getElementById("five").textContent,
+    six : document.getElementById("six").textContent,
+    seven : document.getElementById("seven").textContent,
+    eight : document.getElementById("eight").textContent,
+    nine : document.getElementById("nine").textContent,  
+
+    //operations simbol
+    clear : document.getElementById("clear").textContent,
+    divider : document.getElementById("divider").textContent,
+    multiply : document.getElementById("multiply").textContent,
+    minus : document.getElementById("minus").textContent,
+    plus : document.getElementById("plus").textContent,
+
+}
+
+// test console log for data capture
+
+
+
